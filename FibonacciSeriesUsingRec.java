3f5d02515d814b92382f35b57a00aeb3d24d5ec5
Arshiyak1211 <125182650+Arshiyak1211@users.noreply.github.com>
2023-02-22 08:12:32 -0800

    Add files via upload

---
+++ FibonacciSeriesUsingRec.java
@@ -0,0 +1,23 @@
+package com.programs.in;
+
+public class FibonacciSeriesUsingRec {
+	static int a=0,b=1,c;
+	public static void main(String[] args)
+	{
+		System.out.print(a+" "+b);
+		FibonacciSeriesUsingRec ob=new FibonacciSeriesUsingRec();
+		ob.printFib(10);
+	}
+	void printFib(int i)
+	{
+		if(i>=1)
+		{
+			c=a+b;
+			System.out.print(" "+c);
+			a=b;
+			b=c;
+		printFib(i-1);
+		}
+	}
+
+}

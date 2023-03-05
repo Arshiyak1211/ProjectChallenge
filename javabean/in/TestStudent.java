db2e25cc43361720b8e9bc3398f09ab52c760361
Arshiyak1211 <125182650+Arshiyak1211@users.noreply.github.com>
2023-03-05 03:10:11 -0800

    Add files via upload

---
+++ javabean/in/TestStudent.java
@@ -0,0 +1,13 @@
+package com.javabean.in;
+
+public class TestStudent {
+	public static void main(String[] args)
+	{
+		
+	Student s=new Student();
+	s.setAge(25);
+	int age=s.getAge();
+	System.out.println("Age: "+age);
+
+}
+}
